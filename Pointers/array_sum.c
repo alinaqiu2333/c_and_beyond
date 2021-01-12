2099b08e643367d90b6eb2235230b03c6d49c1d3
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2021-01-11 21:10:13 -0500

    array sum using pointer updtaed

--- Pointers/array_sum.c
+++ Pointers/array_sum.c
@@ -6,8 +6,12 @@ int get_sum(int *L, int size){
 	}
 	return count;
 }
+void change_first_element(int L[5]){
+	L[0] = 100;
+}
 int main(){
 	int L[5] = {1, 2, 3, 4, 5};
+	change_first_element(L);
 	printf("total is %d\n",get_sum(L, 5));
 	return 0;
 }
