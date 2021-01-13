761bb00b5924493b13531ca62ef874b201bdf5bf
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2021-01-12 21:27:53 -0500

    compile using command line added

---
+++ Basics/compile_cmd_line.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	// gcc -Wall -o output_file compile_cmd_line.c 
+	// the above command saves the output into a file called output_file
+	// and it is excutable, this can be tested by using 
+	// ls -F
+	// the above command shows if a file is excutable
+	printf("hello!\n");
+}
