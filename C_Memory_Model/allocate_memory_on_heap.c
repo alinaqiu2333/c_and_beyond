aeb5d3122ddf32a78fd492b2e027682521b1d682
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2021-01-20 07:26:34 -0500

    allocate_memory_on_heap.c updated

--- C_Memory_Model/allocate_memory_on_heap.c
+++ C_Memory_Model/allocate_memory_on_heap.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 int main() {
-    float *rainfall = NULL;
-    // Allocate space for a single floating point number and assign its location to the pointer rainfall.
-    rainfall = (int*)malloc(sizeof(int));
-    *rainfall = 42.5;
+    float *monthly_rainfall = NULL;
+    // Allocate space on the heap for an array of 12 floats and assign it to the pointer monthly_rainfall.
+    monthly_rainfall = malloc(sizeof(float)*12);
+    for (int i = 0; i<12, i++){
+        monthly_rainfall[i]= 0.66;
+    }
     return 0;
 }
