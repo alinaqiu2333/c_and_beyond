4809c925afa4aea8083fd5494231fdaa135d7e54
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2021-01-20 07:19:09 -0500

    allocate_memory_on_heap.c updated

--- C_Memory_Model/allocate_memory_on_heap.c
+++ C_Memory_Model/allocate_memory_on_heap.c
@@ -1,29 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-/* 
- * Return an array of the squares from 1 to max_val.
- */
-int *squares(int max_val) {
-    int *result = malloc(sizeof(int) * max_val);
-    int i;
-    for (i = 1; i <= max_val; i++) {
-        result[i - 1] = i * i ;
-    }
-    return result;
-}
-
+#include <stdio.h>
 
 int main() {
-
-    int *squares_to_10 = squares(10);
-    
-    // let's print them out 
-    int i;
-    for (i = 0; i < 10; i++) {
-        printf("%d\t", squares_to_10[i]);
-    }
-    printf("\n");
-
+    float *rainfall = NULL;
+    // Allocate space for a single floating point number and assign its location to the pointer rainfall.
+    rainfall = (int*)malloc(sizeof(int));
+    *rainfall = 42.5;
     return 0;
 }
